486e27161e7d064c87db8d9118348bf9e338da26
Brent Komer <brent.komer@gmail.com>
2019-03-12 14:36:42 -0400

    adding option for different nonlinearity on network head

--- deep_rl/network/network_heads.py
+++ deep_rl/network/network_heads.py
@@ -135,12 +135,14 @@ class GaussianActorCriticNet(nn.Module, BaseNet):
     def __init__(self,
                  state_dim,
                  action_dim,
+                 gate=F.tanh,
                  phi_body=None,
                  actor_body=None,
                  critic_body=None):
         super(GaussianActorCriticNet, self).__init__()
         self.network = ActorCriticNet(state_dim, action_dim, phi_body, actor_body, critic_body)
         self.std = nn.Parameter(torch.zeros(action_dim))
+        self.gate = gate
         self.to(Config.DEVICE)
 
     def forward(self, obs, action=None):
@@ -148,7 +150,8 @@ class GaussianActorCriticNet(nn.Module, BaseNet):
         phi = self.network.phi_body(obs)
         phi_a = self.network.actor_body(phi)
         phi_v = self.network.critic_body(phi)
-        mean = F.tanh(self.network.fc_action(phi_a))
+        # mean = F.tanh(self.network.fc_action(phi_a))
+        mean = self.gate(self.network.fc_action(phi_a))
         v = self.network.fc_critic(phi_v)
         dist = torch.distributions.Normal(mean, F.softplus(self.std))
         if action is None:
